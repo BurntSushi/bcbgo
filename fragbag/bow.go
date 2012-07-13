15a33acfba464026bf1387253a8263a8f44c4406
Andrew Gallant (Ocelot) <Andrew.Gallant@tufts.edu>
2012-07-13 01:52:12 -0400

    Fragbag package. It abstracts a fragbag library, BOW computation, and BOW operations.

---
+++ fragbag/bow.go
@@ -0,0 +1,123 @@
+package fragbag
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/BurntSushi/bcbgo/pdb"
+)
+
+// BOW represents a bag-of-words vector of size N for a particular fragment
+// library, where N corresponds to the number of fragments in the fragment
+// library.
+type BOW struct {
+	// library corresponds to the fragment library used to compute this
+	// bag-of-words. All BOW operations are only defined when all operands 
+	// belong to the same fragment library.
+	library *Library
+
+	// fragfreqs is a map from fragment number to the number of occurrences of
+	// that fragment in this "bag of words." This map always has size
+	// equivalent to the size of the library.
+	fragfreqs []int16
+}
+
+// NewBow returns a bag-of-words with all fragment frequencies set to 0.
+func (lib *Library) NewBow() BOW {
+	bow := BOW{
+		library:   lib,
+		fragfreqs: make([]int16, lib.Size()),
+	}
+	for i := range lib.fragments {
+		bow.fragfreqs[i] = 0
+	}
+	return bow
+}
+
+// NewPDB returns a bag-of-words describing a pdb file.
+//
+// All protein chains in the PDB file are used.
+func (lib *Library) NewBowPDB(entry *pdb.Entry) BOW {
+	// We don't use the public 'Increment' or 'Add' methods to avoid
+	// excessive allocations.
+	bow := lib.NewBow()
+
+	for _, chain := range entry.Chains {
+		if !chain.ValidProtein() {
+			continue
+		}
+
+		// If this chain is smaller than the fragment size, then we skip it.
+		if len(chain.CaAtoms) < lib.FragmentSize() {
+			continue
+		}
+
+		// Otherwise, the chain is bigger than the fragment size. So pick the
+		// best fragment for each overlapping N-mer, where N is the fragment
+		// size.
+		for i := 0; i <= len(chain.CaAtoms)-lib.FragmentSize(); i++ {
+			atoms := chain.CaAtoms[i : i+lib.FragmentSize()]
+			if fragNum := lib.BestFragment(atoms); fragNum >= 0 {
+				bow.fragfreqs[fragNum] += 1
+			}
+		}
+	}
+	return bow
+}
+
+// Increment will increment the frequency of the given fragment number by 1.
+// If a fragment of fragNum does not exist, Increment will panic.
+func (bow BOW) Increment(fragNum int) {
+	bow.library.mustExist(fragNum)
+	bow.fragfreqs[fragNum] += 1
+}
+
+// Add performs an add operation on each fragment frequency and returns
+// a new BOW. Add will panic if the operands came from different fragment
+// libraries.
+func (bow1 BOW) Add(bow2 BOW) BOW {
+	mustHaveSameLibrary(bow1, bow2)
+
+	sum := bow1.library.NewBow()
+	for i := 0; i < sum.library.Size(); i++ {
+		sum.fragfreqs[i] = bow1.fragfreqs[i] + bow2.fragfreqs[i]
+	}
+	return sum
+}
+
+// String returns a string representation of the BOW vector. Only fragments
+// with non-zero frequency are emitted.
+//
+// The output looks like '{fragNum: frequency, fragNum: frequency, ...}'.
+// i.e., '{1: 4, 3: 1}' where all fragment numbers except '1' and '3' have
+// a frequency of zero.
+func (bow BOW) String() string {
+	pieces := make([]string, 0, 10)
+	for i := 0; i < bow.library.Size(); i++ {
+		freq := bow.fragfreqs[i]
+		if freq > 0 {
+			pieces = append(pieces, fmt.Sprintf("%d: %d", i, freq))
+		}
+	}
+	return fmt.Sprintf("{%s}", strings.Join(pieces, ", "))
+}
+
+// mustHaveSameLibrary panics if any bow in bows belongs to a different
+// library than any other bow in bows. (Libraries are compared using pointer
+// equality.)
+func mustHaveSameLibrary(bows ...BOW) {
+	var lib *Library = nil
+	for _, bow := range bows {
+		if bow.library == nil {
+			panic(fmt.Sprintf("A BOW does not belong to any library."))
+		}
+		if lib == nil {
+			lib = bow.library
+			continue
+		}
+		if lib != bow.library {
+			panic(fmt.Sprintf("A BOW belongs to library '%s', but another "+
+				"BOW belongs to library '%s'.", bow.library, lib))
+		}
+	}
+}
---
+++ fragbag/lib.go
@@ -0,0 +1,219 @@
+package fragbag
+
+import (
+	"fmt"
+	"io/ioutil"
+	"path"
+	"sort"
+	"strconv"
+	"strings"
+
+	"github.com/BurntSushi/bcbgo/pdb"
+	"github.com/BurntSushi/bcbgo/rmsd"
+)
+
+// Library represents a Fragbag fragment library. Fragbag fragment libraries
+// are fixed both in the number of fragments and in the size of each fragment.
+//
+// A Fragbag library traditionally existed as one monolithic '.brk' file, but
+// this implementation uses a slightly different form called General Fragment
+// Form. The key distinction is that a library is represented on disk as a
+// directory where each fragment in the library corresponds to a single file
+// with a 16-bit integer name. (i.e., "4" and "2000" are valid fragment file
+// names, but "one" and "1000000" are not.)
+//
+// The only invariants imposed by this package is that every fragment in a
+// library must be the same size, and fragment numbers must start from 1 and
+// be contiguous.
+//
+// Files in a Fragbag library directory that do not correspond to 16-bit
+// integer names are simply ignored.
+type Library struct {
+	Path         string
+	fragmentSize int
+	fragments    []LibFragment
+}
+
+// NewLibrary constucts a new Fragbag library given a path to the directory
+// containing the fragment files. It will return an error if the directory
+// or any of the fragment files aren't readable, if there are no fragment files,
+// or if there is any variability among fragment sizes in all fragments.
+func NewLibrary(path string) (*Library, error) {
+	infos, err := ioutil.ReadDir(path)
+	if err != nil {
+		return nil, err
+	}
+
+	lib := &Library{
+		Path:         path,
+		fragmentSize: 0,
+		fragments:    nil,
+	}
+
+	// Add all files in the directory that have names that are translatable
+	// to 16-bit integers.
+	fragNums := make([]int, 0, 100)
+	for _, info := range infos {
+		if fragNum64, err := strconv.ParseInt(info.Name(), 10, 16); err == nil {
+			fragNums = append(fragNums, int(fragNum64))
+		}
+	}
+
+	// If there aren't any fragments, return an error. No empty libraries!
+	if len(fragNums) == 0 {
+		return nil, fmt.Errorf("The library at '%s' does not contain any "+
+			"fragments.", path)
+	}
+
+	// Check that the fragment numbering is contiguous, and populate
+	// our fragments slice.
+	lib.fragments = make([]LibFragment, len(fragNums))
+	sort.Sort(sort.IntSlice(fragNums))
+	lastNum := -1
+	for _, fragNum := range fragNums {
+		if lastNum != fragNum-1 {
+			if lastNum == -1 {
+				return nil, fmt.Errorf("Fragment files must start numbering "+
+					"at 1 and be contiguous. However, the first fragment "+
+					"number in library '%s' is %d.",
+					path, fragNum)
+			}
+			return nil, fmt.Errorf("Fragment files must start numbering "+
+				"at 1 and be contiguous. However, a fragment of number "+
+				"%d was found after a fragment of number %d.",
+				fragNum, lastNum)
+		}
+		lastNum = fragNum
+
+		lib.fragments[fragNum], err = lib.newLibFragment(fragNum)
+		if err != nil {
+			return nil, fmt.Errorf("The fragment file '%d' in the '%s' "+
+				"library could not be read as a PDB file: %s",
+				fragNum, path, err)
+		}
+	}
+
+	// Set the fragment size of this library to the size of one of the
+	// fragments. Then make sure every other fragment has the same size.
+	for _, frag := range lib.fragments {
+		size := frag.realSize()
+		if lib.fragmentSize == 0 {
+			lib.fragmentSize = size
+		} else {
+			if size != lib.fragmentSize {
+				return nil, fmt.Errorf("In the library at '%s', fragment %d "+
+					"has a size of %d, but another fragment has a size of %d.",
+					path, frag.Ident, size, lib.fragmentSize)
+			}
+		}
+	}
+
+	return lib, nil
+}
+
+// Size returns the number of fragments in the library.
+func (lib *Library) Size() int {
+	return len(lib.fragments)
+}
+
+// FragmentSize returns the size of each fragment.
+func (lib *Library) FragmentSize() int {
+	return lib.fragmentSize
+}
+
+// Fragment returns a LibFragment corresponding to the fragment number
+// fragNum. Fragment will panic if such a fragment does not exist.
+func (lib *Library) Fragment(fragNum int) LibFragment {
+	if fragNum >= 0 && fragNum < len(lib.fragments) {
+		return lib.fragments[fragNum]
+	}
+	panic(fmt.Sprintf("Fragment number %d does not exist in the "+
+		"'%s' fragment library.", fragNum, lib))
+}
+
+// BestFragment runs Kabsch using the provided PDB argument against all 
+// fragments in the library and returns the fragment number with the best RMSD.
+//
+// BestFragment panics if the length of atoms is not equivalent to the
+// fragment size of the library.
+func (lib *Library) BestFragment(atoms pdb.Atoms) int {
+	if len(atoms) != lib.FragmentSize() {
+		panic(fmt.Sprintf("BestFragment can only be called with a list of "+
+			"atoms with length equivalent to the fragment size of the "+
+			"library. The length of the list given is %d, but the fragment "+
+			"size of the library is %d.", len(atoms), lib.FragmentSize()))
+	}
+
+	bestRmsd, bestFragNum := 0.0, -1
+	for _, frag := range lib.fragments {
+		testRmsd := rmsd.RMSD(atoms, frag.OneChain().CaAtoms)
+		if bestFragNum == -1 || testRmsd < bestRmsd {
+			bestRmsd, bestFragNum = testRmsd, frag.Ident
+		}
+	}
+	return bestFragNum
+}
+
+// String returns a string with the name of the library (base name of the 
+// library directory), the number of fragments in the library and the size
+// of each fragment.
+func (lib *Library) String() string {
+	return fmt.Sprintf("%s (%d, %d)",
+		path.Base(lib.Path), len(lib.fragments), lib.fragmentSize)
+}
+
+// mustExist will panic if the given fragment number does not exist.
+func (lib *Library) mustExist(fragNum int) {
+	if fragNum < 0 || fragNum >= len(lib.fragments) {
+		panic(fmt.Sprintf("Fragment number %d does not exist in library '%s'.",
+			fragNum, lib))
+	}
+}
+
+// LibFragment corresponds to a single fragment file in a fragment library.
+// It holds the fragment number identifier and embeds a PDB entry.
+type LibFragment struct {
+	library *Library
+	Ident   int
+	*pdb.Entry
+}
+
+// newLibFragment creates a new LibFragment with a particular fragment number
+// given a fragment library. A pdb.Entry is also created and embedded with
+// the Location corresponding to a file path concatenation of the library path
+// and the fragment number.
+func (lib *Library) newLibFragment(fragNum int) (LibFragment, error) {
+	path := path.Join(lib.Path, fmt.Sprintf("%d", fragNum))
+	entry, err := pdb.New(path)
+	if err != nil {
+		return LibFragment{}, err
+	}
+	return LibFragment{
+		library: lib,
+		Ident:   fragNum,
+		Entry:   entry,
+	}, nil
+}
+
+// String returns the fragment number, library and its corresponding atoms.
+func (frag LibFragment) String() string {
+	chain := frag.OneChain()
+	atoms := make([]string, len(chain.CaAtoms))
+	for i, atom := range chain.CaAtoms {
+		atoms[i] = fmt.Sprintf("\t%s", atom)
+	}
+	return fmt.Sprintf("> %d (%s)\n%s",
+		frag.Ident, frag.library, strings.Join(atoms, "\n"))
+}
+
+// realSize reads the fragment file and counts the number of occurrences of
+// "ATOM" to report the actual size of the fragment.
+//
+// NewLibrary enforces the invariant that all fragments in the library have
+// the same size.
+//
+// If 'realSize' returns an error, then the fragment file wasn't readable.
+// The 'NewLibrary' constructor should then fail and return this error.
+func (frag LibFragment) realSize() int {
+	return len(frag.OneChain().CaAtoms)
+}
