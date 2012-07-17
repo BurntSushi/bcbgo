b20ff09df13e8ea087e937fd609bec8e61e9664e
Andrew Gallant (Ocelot) <Andrew.Gallant@tufts.edu>
2012-07-16 20:40:55 -0400

    Add documentation for package rmsd.

---
+++ rmsd/doc.go
@@ -0,0 +1,8 @@
+/*
+Package rmsd implements a version of the Kabsch algorithm that is described
+in detail here: http://cnx.org/content/m11608/latest/
+
+A convenience function for computing the RMSD of residue ranges from two PDB
+files is also provided.
+*/
+package rmsd
