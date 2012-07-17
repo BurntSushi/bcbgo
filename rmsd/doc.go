b3239e49cb77f8dac7bd968d73aab42cfcd51082
Andrew Gallant (Ocelot) <Andrew.Gallant@tufts.edu>
2012-07-16 20:42:23 -0400

    Make first sentence a bit more descriptive. And don't put a link in the first sentence.

--- rmsd/doc.go
+++ rmsd/doc.go
@@ -1,6 +1,7 @@
 /*
-Package rmsd implements a version of the Kabsch algorithm that is described
-in detail here: http://cnx.org/content/m11608/latest/
+Package rmsd implements a version of the Kabsch algorithm to compute the
+minimal RMSD between two equal length sets of atoms. The exact algorithm
+implement is described in detail here: http://cnx.org/content/m11608/latest/.
 
 A convenience function for computing the RMSD of residue ranges from two PDB
 files is also provided.
