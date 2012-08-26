5439dd998beb93e6b8509d7fb3087e4ef4659b22
Andrew Gallant (Ocelot) <Andrew.Gallant@tufts.edu>
2012-08-25 23:41:01 -0400

    Starting command to create PDB index.

--- cmd/create-pdb-index/main.go
+++ cmd/create-pdb-index/main.go
@@ -7,9 +7,14 @@ import (
 	"fmt"
 	"os"
 	"path"
+	"runtime"
 
 	"github.com/BurntSushi/bcbgo/fragbag"
 	"github.com/BurntSushi/bcbgo/pdb"
+)
+
+var (
+	flagGoMaxProcs = runtime.NumCPU()
 )
 
 func main() {
@@ -26,21 +31,32 @@ func main() {
 	}
 	fmt.Printf("Using library %s.\n", lib)
 
-	for _, pdbfile := range flag.Args()[1:] {
+	pdbFiles := flag.Args()[1:]
+	entries := make([]*pdb.Entry, len(pdbFiles))
+	for i, pdbfile := range flag.Args()[1:] {
 		entry, err := pdb.New(pdbfile)
 		if err != nil {
 			fmt.Println(err)
 			return
 		}
+		entries[i] = entry
+	}
+	bows := lib.NewBowsPDBList(entries...)
+	for i, entry := range entries {
 		fmt.Printf("Computing the bag-of-words vector for %s.\n", entry.Name())
-		fmt.Println(lib.NewBowPDB(entry))
+		fmt.Println(bows[i])
+		// fmt.Println(lib.NewBowPDB(entry)) 
 		fmt.Println("----------------------------------------------")
 	}
 }
 
 func init() {
+	flag.IntVar(&flagGoMaxProcs, "p", flagGoMaxProcs,
+		"The maximum number of CPUs that can be executing simultaneously.")
 	flag.Usage = usage
 	flag.Parse()
+
+	runtime.GOMAXPROCS(flagGoMaxProcs)
 }
 
 func usage() {
