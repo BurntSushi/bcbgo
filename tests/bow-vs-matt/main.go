93e25b0ad28778beb8602e210fb4868737cacd2c
Andrew Gallant (Ocelot) <Andrew.Gallant@tufts.edu>
2012-10-23 01:37:14 -0400

    First test.

---
+++ tests/bow-vs-matt/main.go
@@ -0,0 +1,198 @@
+// test bow-vs-matt compares the structural neighbors detected by a search
+// on BOW vectors verus the structural neighbors detected by the Matt
+// structural aligner.
+//
+// The test only produces the output of the comparison, but does not say how
+// similar they are.
+//
+// The comparison is that of ordering. Namely, given some protein chain A,
+// find the closest structural neighbors returned by two different methods:
+// structural alignment and BOW searching.
+//
+// This is done by first creating a BOW database from the given list of PDB
+// files. (This requires that 'create-pdb-db' is in your PATH.) Then, output
+// a file for every protein chain that contains a ordering of all structural
+// neighbors using both search techniques.
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"os/exec"
+	"path"
+	"runtime"
+	"runtime/pprof"
+	"strings"
+	"text/tabwriter"
+
+	"github.com/BurntSushi/bcbgo/bowdb"
+	"github.com/BurntSushi/bcbgo/matt"
+)
+
+type results []result
+
+type result struct {
+	entry string
+	chain byte
+	results bowdb.SearchResults
+}
+
+var (
+	flagCpuProfile = ""
+	flagGoMaxProcs = runtime.NumCPU()
+	flagQuiet      = false
+	flagInverted   = false
+)
+
+func main() {
+	if flag.NArg() < 2 {
+		usage()
+	}
+	dbPath := flag.Arg(0)
+	fragLibDir := flag.Arg(1)
+	pdbFiles := flag.Args()[2:]
+
+	if err := createBowDb(dbPath, fragLibDir, pdbFiles); err != nil {
+		fatalf("%s\n", err)
+	}
+
+	var searchType int
+	if flagInverted {
+		searchType = bowdb.SearchInverted
+	} else {
+		searchType = bowdb.SearchFull
+	}
+	db, err := bowdb.Open(dbPath, searchType)
+	if err != nil {
+		fatalf("%s\n", err)
+	}
+
+	if len(flagCpuProfile) > 0 {
+		f, err := os.Create(flagCpuProfile)
+		if err != nil {
+			fatalf("%s\n", err)
+		}
+		pprof.StartCPUProfile(f)
+		defer pprof.StopCPUProfile()
+	}
+
+	bowOpts := bowdb.DefaultSearchOptions
+	mattOpts := matt.DefaultConfig
+	mattOpts.Verbose = false
+
+	chains := createChains(pdbFiles)
+	mattArgs := createMattArgs(chains)
+
+	tabw := tabwriter.NewWriter(os.Stdout, 0, 4, 4, ' ', 0)
+	header := []byte(
+		"BOW entry\t" +
+			"BOW chain\t" +
+			"BOW dist\t" +
+			"Matt entry\t" +
+			"Matt chain\t" +
+			"Matt dist\n")
+	for i, chain := range chains {
+		marg := mattArgs[i]
+		bow := db.Library.NewBowChain(chain)
+
+		bowOrdered, err := getBowOrdering(db, bowOpts, bow)
+		if err != nil {
+			errorf("Could not get BOW ordering for %s (chain %c): %s\n",
+				chain.Entry.IdCode, chain.Ident, err)
+			continue
+		}
+
+		mattOrdered, err := getMattOrdering(mattOpts, marg, mattArgs)
+		if err != nil {
+			errorf("Could not get Matt ordering for %s (chain %c): %s\n",
+				chain.Entry.IdCode, chain.Ident, err)
+			continue
+		}
+
+		fmt.Printf("Ordering for %s (chain %c)\n",
+			chain.Entry.IdCode, chain.Ident)
+
+		compared := comparison([2]ordering{bowOrdered, mattOrdered})
+		tabw.Write(header)
+		tabw.Write([]byte(compared.String()))
+		tabw.Flush()
+		fmt.Println("\n")
+	}
+
+	if err := db.ReadClose(); err != nil {
+		fatalf("There was an error closing the database: %s\n", err)
+	}
+}
+
+func createBowDb(dbPath string, fragLibDir string, pdbFiles []string) error {
+	if _, err := os.Stat(dbPath); err == nil || os.IsExist(err) {
+		return nil
+	}
+
+	args := []string{dbPath, fragLibDir}
+	args = append(args, pdbFiles...)
+	cmd := exec.Command("create-pdb-db", args...)
+
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		return fmt.Errorf("Error running '%s': %s.",
+			strings.Join(cmd.Args, " "), err)
+	}
+	return nil
+}
+
+func init() {
+	flag.BoolVar(&flagInverted, "inverted", flagInverted,
+		"When set, the search will use an inverted index.")
+	flag.StringVar(&flagCpuProfile, "cpuprofile", flagCpuProfile,
+		"When set, a CPU profile will be written to the file provided.")
+	flag.IntVar(&flagGoMaxProcs, "p", flagGoMaxProcs,
+		"The maximum number of CPUs that can be executing simultaneously.")
+	flag.BoolVar(&flagQuiet, "quiet", flagQuiet,
+		"When set, no progress bar will be shown.\n"+
+			"\tErrors will still be printed to stderr.")
+	flag.Usage = usage
+	flag.Parse()
+
+	runtime.GOMAXPROCS(flagGoMaxProcs)
+}
+
+func usage() {
+	errorf("Usage: %s database-path frag-lib-directory "+
+		"query-pdb-file [query-pdb-file ...]\n",
+		path.Base(os.Args[0]))
+	flag.PrintDefaults()
+	os.Exit(1)
+}
+
+func verbosef(format string, v ...interface{}) {
+	if flagQuiet {
+		return
+	}
+	fmt.Fprintf(os.Stdout, format, v...)
+}
+
+func errorf(format string, v ...interface{}) {
+	fmt.Fprintf(os.Stderr, format, v...)
+}
+
+func fatalf(format string, v ...interface{}) {
+	errorf(format, v...)
+	os.Exit(1)
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
---
+++ tests/bow-vs-matt/orderings.go
@@ -0,0 +1,127 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+	"strings"
+
+	"github.com/BurntSushi/bcbgo/bowdb"
+	"github.com/BurntSushi/bcbgo/fragbag"
+	"github.com/BurntSushi/bcbgo/matt"
+	"github.com/BurntSushi/bcbgo/pdb"
+)
+
+type comparison [2]ordering
+
+func (c comparison) String() string {
+	least := min(len(c[0]), len(c[1]))
+	lines := make([]string, least)
+	for i := 0; i < least; i++ {
+		lines[i] = fmt.Sprintf("%s\t%s", c[0][i], c[1][i])
+	}
+	return strings.Join(lines, "\n")
+}
+
+type ordering []chain
+
+func (o ordering) Less(i, j int) bool {
+	return o[i].dist < o[j].dist
+}
+
+func (o ordering) Len() int {
+	return len(o)
+}
+
+func (o ordering) Swap(i, j int) {
+	o[i], o[j] = o[j], o[i]
+}
+
+type chain struct {
+	idCode string
+	ident byte
+	dist float64
+}
+
+func (c chain) String() string {
+	return fmt.Sprintf("%s\t%c\t%0.4f", c.idCode, c.ident, c.dist)
+}
+
+func getBowOrdering(
+	db *bowdb.DB, opts bowdb.SearchOptions, bow fragbag.BOW) (ordering, error) {
+
+	results, err := db.Search(opts, bow)
+	if err != nil {
+		return nil, err
+	}
+
+	ordered := make(ordering, len(results.Results))
+	for i, result := range results.Results {
+		ordered[i] = chain{
+			idCode: result.IdCode,
+			ident: result.ChainIdent,
+			dist: result.Euclid,
+		}
+	}
+
+	// These are already sorted.
+	return ordered, nil
+}
+
+func getMattOrdering(
+	conf matt.Config, query matt.PDBArg, rest []matt.PDBArg) (ordering, error) {
+
+	argsets := make([][]matt.PDBArg, len(rest))
+	for i, target := range rest {
+		argsets[i] = []matt.PDBArg{query, target}
+	}
+
+	results, errs := conf.RunAll(argsets)
+	ordered := make(ordering, 0, len(rest))
+	for i, result := range results {
+		target := argsets[i][1]
+		if errs[i] != nil {
+			errorf("Could not get Matt RMSD for %s (chain %c) against "+
+				"%s (chain %c): %s",
+				query.IdCode, query.Chain, target.IdCode, target.Chain, errs[i])
+			continue
+		}
+		ordered = append(ordered, chain{
+			idCode: target.IdCode,
+			ident: target.Chain,
+			dist: result.RMSD,
+		})
+	}
+	sort.Sort(ordered)
+	return ordered, nil
+}
+
+func createMattArgs(chains []*pdb.Chain) []matt.PDBArg {
+	args := make([]matt.PDBArg, len(chains))
+	for i, chain := range chains {
+		args[i] = matt.PDBArg{
+			Location: chain.Entry.Path,
+			IdCode: chain.Entry.IdCode,
+			Chain: chain.Ident,
+		}
+	}
+	return args
+}
+
+func createChains(pdbFiles []string) []*pdb.Chain {
+	chains := make([]*pdb.Chain, 0, len(pdbFiles))
+	for _, pdbFile := range pdbFiles {
+		entry, err := pdb.New(pdbFile)
+		if err != nil {
+			errorf("Could not parse PDB file '%s' because: %s\n", pdbFile, err)
+			continue
+		}
+
+		for _, chain := range entry.Chains {
+			if !chain.ValidProtein() {
+				continue
+			}
+			chains = append(chains, chain)
+		}
+	}
+	return chains
+}
